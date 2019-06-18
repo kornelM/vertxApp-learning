8e07372fec0d8ccccdcfa201fd25b8b0fe7ebf82
kornelM <makusz07@gmail.com>
2019-06-18 22:28:33 +0200

    Added external configuration in App class

--- src/main/java/com/autobot/App.java
+++ src/main/java/com/autobot/App.java
@@ -1,5 +1,6 @@
 package com.autobot;
 
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
@@ -7,18 +8,25 @@ public class App extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> fut) {
-        ConfigRetriver
-        vertx.createHttpServer()
-                .requestHandler(r ->
-                        r.response().end("<h1>Hello Kornel from my first Vert.x application</h1>"))
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+        retriever.getConfig(
+                config -> {
+                    if (config.failed()) {
+                        fut.fail(config.cause());
+                    } else {
+                        vertx.createHttpServer()
+                                .requestHandler(r ->
+                                        r.response().end("<h1>Hello Kornel from my first Vert.x application</h1>"))
 
-                .listen(config().getInteger("HTTP_PORT", 8080),
-                        result -> {
-                            if (result.succeeded()) {
-                                fut.complete();
-                            } else {
-                                fut.fail(result.cause());
-                            }
-                        });
+                                .listen(config().getInteger("HTTP_PORT", 8080),
+                                        result -> {
+                                            if (result.succeeded()) {
+                                                fut.complete();
+                                            } else {
+                                                fut.fail(result.cause());
+                                            }
+                                        });
+                    }
+                });
     }
 }
